a4dfb2bf55336ae6dbdae25e5f560094d7b475fe
Valentin Drany <navshop@ukr.net>
2016-10-04 15:46:54 +0300

    Fix for IE
    Tested on IE11+

--- js/paralax.js
+++ js/paralax.js
@@ -1,14 +1,16 @@
-(() =>
+(function()
 {
 	"use strict"
 
 	var $body = document.querySelector('.bg_paralax')
 
 	window.addEventListener("scroll", function(event)
-	{
-		var scrollY = event.currentTarget.scrollY
-        	var backgroundPositionY = scrollY/5 + 'px'
+    	{
+        	var scrollY = event.currentTarget.pageYOffset
+        	var backgroundPositionY = scrollY + 'px'
 
-		$body.style.backgroundPosition = "0px " + backgroundPositionY
-	})
+        	$body.style.backgroundPosition = "0px " + backgroundPositionY
+
+        	event.preventDefault()
+    	})
 })()
